92df9ec46fff30fc26dbf8c1c3519e60037975e1
Paul <77917593+norbfi@users.noreply.github.com>
2022-09-25 14:19:50 +0200

    Schönheitsfehler behoben

--- Aufgabe 3 Sudokopie/Main.py
+++ Aufgabe 3 Sudokopie/Main.py
@@ -1,9 +1,9 @@
 from DataReadingSudoku import DataReading
 
 sudokuOrignal = (DataReading('Aufgabe 3 Sudokopie\Beispieldaten\Sudoku0.txt', True))
-SudokuKopie = (DataReading('Aufgabe 3 Sudokopie\Beispieldaten\Sudoku0.txt', False))
+sudokuKopie = (DataReading('Aufgabe 3 Sudokopie\Beispieldaten\Sudoku0.txt', False))
 
-print(sudokuOrignal, "\n \n" , SudokuKopie)
+print(sudokuOrignal, "\n \n" , sudokuKopie)
 
 
 
