fbdb0f8f6843ac8087264f3a68a1fa80655eea87
Ben <Ben@d.de>
2022-11-01 22:08:07 +0100

    ab jetzt muss überlegt werden

--- Aufgabe 2 Verzinkt/verzinkt.py
+++ Aufgabe 2 Verzinkt/verzinkt.py
@@ -3,9 +3,14 @@ from PIL import Image, ImageDraw
 
 
 width, height = 100, 100
+weiss = (255, 255, 255) 
+
 
 img = Image.new("RGB", (100, 100), "black")
 
-img.show()
+img.putpixel((1,1), weiss)
 
-#TODO Pixelbild definition
+img.save("img.png")
+
+
+#TODO farb algorithmus 
