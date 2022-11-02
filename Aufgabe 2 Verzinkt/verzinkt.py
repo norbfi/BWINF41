8379ca02f461ac78b0ab02c51d0b5d915349daf7
Ben <Ben@d.de>
2022-11-02 11:38:13 +0100

    changes manchanges

--- Aufgabe 2 Verzinkt/verzinkt.py
+++ Aufgabe 2 Verzinkt/verzinkt.py
@@ -1,16 +1,30 @@
 from PIL import Image, ImageDraw
+import random 
+import time 
 
 
-
+#bild erzeugen
 width, height = 100, 100
-weiss = (255, 255, 255) 
-
-
 img = Image.new("RGB", (100, 100), "black")
 
-img.putpixel((1,1), weiss)
+#variables for color 
+right = (185, 171, 171)
+left = (185, 171, 171)
+top = (132, 125, 125)
+bottom = (213, 211, 211)
+
+startpoint = (50,50)
+
+#simulation code
+
+
+#for i in range(0,50):
+ #   for j in range(0,50):
+  #      img.putpixel((i, j), bottom)
+
+
 
 img.save("img.png")
 
 
-#TODO farb algorithmus 
+#TODO farb algorithmus
