979ed3a266e5e40796c362c74c22b6d1250fb2c9
Ben <Ben@d.de>
2022-11-02 12:00:44 +0100

    progress ye

--- Aufgabe 2 Verzinkt/verzinkt.py
+++ Aufgabe 2 Verzinkt/verzinkt.py
@@ -2,7 +2,6 @@ from PIL import Image, ImageDraw
 import random 
 import time 
 
-
 #bild erzeugen
 width, height = 100, 100
 img = Image.new("RGB", (100, 100), "black")
@@ -13,18 +12,21 @@ left = (185, 171, 171)
 top = (132, 125, 125)
 bottom = (213, 211, 211)
 
-startpoint = (50,50)
-
 #simulation code
+def crisgrow(spwnpnt):
+    pass
+
+
+def spwnpnt():
+    spwnpnt = (random.randint(0,100),random.randint(0,100))
+    return spwnpnt 
+
 
 
 #for i in range(0,50):
  #   for j in range(0,50):
   #      img.putpixel((i, j), bottom)
 
-
-
 img.save("img.png")
-
 
 #TODO farb algorithmus
