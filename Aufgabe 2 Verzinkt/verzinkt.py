980e6516dc411ff7b4d2cd5479bbdeda1c35dfb5
Ben <75950537+l3l342@users.noreply.github.com>
2022-11-16 01:04:38 +0100

    Add files via upload

--- Aufgabe 2 Verzinkt/verzinkt.py
+++ Aufgabe 2 Verzinkt/verzinkt.py
@@ -3,14 +3,14 @@ import random
 import time 
 
 #bild erzeugen
-width, height = 100, 100
-img = Image.new("RGB", (100, 100), "black")
+img = Image.new("RGB", (101, 101), "black")
 
 #variables for color 
 right = (185, 171, 171)
 left = (185, 171, 171)
 top = (132, 125, 125)
 bottom = (213, 211, 211)
+
 
 #random spawnpoint
 def spwnpnt():
@@ -20,10 +20,18 @@ def spwnpnt():
 #simulation code
 def crisgrow(spwnpnt):
     spwnpnt = spwnpnt()
+    
+    img.putpixel(spwnpnt, (100,0,0))
+    for x in range(spwnpnt[0]):
+        for y in range(spwnpnt[1]):
 
-    for i in range(spwnpnt[0], 100):
-        for j in range(spwnpnt[1], 100):
-            img.putpixel((i, j), bottom)
+            if img.getpixel((x + 1, y)) == (0,0,0):
+                img.putpixel((x + 1, y), left)
+            else:
+                break
+
+            y = y+1
+        x = x+1
             
 
 
@@ -31,6 +39,8 @@ def crisgrow(spwnpnt):
  #   for j in range(0,50):
   #      img.putpixel((i, j), bottom)
 
+
+crisgrow(spwnpnt)
 crisgrow(spwnpnt)
 
 
